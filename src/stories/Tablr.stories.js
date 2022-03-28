3f1f80fc698a9fb2b3d69e8503e6c60615351ac1
HoangZoan <d.hoang1906@gmail.com>
2022-03-28 16:49:35 +0700

    Initial commit

---
+++ src/stories/Tablr.stories.js
@@ -0,0 +1,24 @@
+import Tablr from "../Tablr";
+
+export default {
+  title: "Tablr - A Customizable React Table Component",
+  component: Tablr,
+  argTypes: {
+    rows: { control: "" },
+    headers: { control: "" },
+  },
+};
+
+const Template = (args) => <Tablr {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  rows: [
+    ["This", "is", "just", "a", "test"],
+    ["This", "is", "also", "a", "test"],
+    ["Just", "a", "little", "more", "data"],
+    ["Row", "number", "four", "right", "here"],
+  ],
+  headers: ["Col 1", "Col 2", "Col 3", "Col 4", "Col 5"],
+};
